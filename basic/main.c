4ab1198dcc0b9af7d46883ffc50e1ec45cf6a88c
Jim Geist <jimge@rodentia.net>
2017-07-19 16:19:52 -0700

    Initial Commit

---
+++ basic/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  basic
+//
+//  Created by Jim Geist on 7/19/17.
+//  Copyright © 2017 Ridge Lift Software. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
