4284b3451f50ce321aae350c264caaca60f52c4d
Jim Geist <jimge@rodentia.net>
2017-07-23 11:37:33 -0700

    Bunch more built in functions

--- basic/builtins.c
+++ basic/builtins.c
@@ -19,14 +19,26 @@ struct builtin
 };
 
 static value *builtin_abs(runtime *rt, value **argv);
+static value *builtin_cos(runtime *rt, value **argv);
+static value *builtin_ln(runtime *rt, value **argv);
+static value *builtin_log(runtime *rt, value **argv);
+static value *builtin_sin(runtime *rt, value **argv);
+static value *builtin_tan(runtime *rt, value **argv);
 
 static builtin builtins[] =
 {
     { "ABS", &builtin_abs, 1, { TYPE_NUMBER } },
+    { "COS", &builtin_cos, 1, { TYPE_NUMBER } },
+    { "LN", &builtin_ln, 1, { TYPE_NUMBER } },
+    { "LOG", &builtin_log, 1, { TYPE_NUMBER } },
+    { "SIN", &builtin_sin, 1, { TYPE_NUMBER } },
+    { "TAN", &builtin_tan, 1, { TYPE_NUMBER } },
 
     { NULL, NULL, 0, {}}
 };
 
+/* Execute a built-in function
+ */
 value *builtin_execute(runtime *rt, const char *id, int argc, value **argv)
 {
     int i = 0;
@@ -45,7 +57,44 @@ value *builtin_execute(runtime *rt, const char *id, int argc, value **argv)
     return builtins[i].execute(rt, argv);
 }
 
+/* Absolute value
+ */
 value *builtin_abs(runtime *rt, value **argv)
 {
     return value_alloc_number(fabs(argv[0]->number));
+}
+
+/* Cosine
+ */
+value *builtin_cos(runtime *rt, value **argv)
+{
+    return value_alloc_number(cos(argv[0]->number));
+}
+
+/* Log base e
+ */
+value *builtin_ln(runtime *rt, value **argv)
+{
+    return value_alloc_number(log(argv[0]->number));
+}
+
+/* Log base 10
+ */
+value *builtin_log(runtime *rt, value **argv)
+{
+    return value_alloc_number(log10(argv[0]->number));
+}
+
+/* Sine
+ */
+value *builtin_sin(runtime *rt, value **argv)
+{
+    return value_alloc_number(sin(argv[0]->number));
+}
+
+/* Tangent
+ */
+value *builtin_tan(runtime *rt, value **argv)
+{
+    return value_alloc_number(tan(argv[0]->number));
 }
